b1e4464573d4f2f540fe4be26e34def5faa7aea9
Daniel Barclay <barclaysd@me.com>
2018-12-01 21:07:36 +0000

    mergeMap for observable request response, limited concurrency and error handling/retry

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Scrape the web!
 
 Use case:
-* Scrape a list of a movies from IMDB
-    - The title must be a movie
-    - Must be of genre 'comedy'
-    - Rating of 7 or greater
+* Scrape a list of a films from IMDB
+    - The listing must be a film
+    - Must be of genre type 'drama'
+    - Rating of 7.5 or greater
--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-const {BehaviorSubject, from} = require('rxjs');
-const {map, distinct, filter, mergeMap} = require('rxjs/operators');
+const {BehaviorSubject, from, of, timer, throwError} = require('rxjs');
+const {map, distinct, filter, mergeMap, retry, catchError, finalize, share, retryWhen} = require('rxjs/operators');
 const normalizeUrl = require('normalize-url');
 const rp = require('request-promise-native');
 const {resolve} = require('url');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
+// key web scraping properties
 const baseUrl = 'https://imdb.com';
+const maxConcurrentReq = 10;
+const maxRetries = 5;
+
+const genericRetryStrategy = ({
+                                  maxRetryAttempts,
+                                  scalingDuration,
+                                  excludedStatusCodes
+                              }) => attempts => {
+    return attempts.pipe(
+        mergeMap((error, i) => {
+            const retryAttempt = i + 1;
+            // if maximum number of retries have been met
+            // or response is a status code we don't wish to retry, throw error
+            if (
+                retryAttempt > maxRetryAttempts ||
+                excludedStatusCodes.find(e => e === error.status)
+            ) {
+                return throwError(error);
+            }
+            console.log(
+                `Attempt ${retryAttempt}: retrying in ${retryAttempt *
+                scalingDuration}ms`
+            );
+            // retry after 1s, 2s, etc...
+            return timer(retryAttempt * scalingDuration);
+        }),
+        finalize(() => console.log('We are done!'))
+    );
+};
+
 
 // starting point for all crawling operations
 const allUrl$ = new BehaviorSubject(baseUrl);
 
 // scrape each URL only once
-const uniqleUrl$ = allUrl$.pipe(
+const uniqueUrl$ = allUrl$.pipe(
     // just crawl IMDB url
     filter(url => url.includes(baseUrl)),
     // normalise url for comparison
@@ -22,28 +54,71 @@ const uniqleUrl$ = allUrl$.pipe(
 
 // make request to each URL and map content into an observable
 
-const urlAndDOM$ = uniqleUrl$.pipe(
-    mergeMap(url => {
-        return from(rp(url)).pipe(
-            map(html => cheerio.load(html)),
-            // add URL to the result
-            map($ => ({
-                $,
-                url
-            }))
-        );
-    })
+const urlAndDOM$ = uniqueUrl$.pipe(
+    mergeMap(
+        url => {
+            return from(rp(url)).pipe(
+                retryWhen(
+                    genericRetryStrategy({
+                        maxRetryAttempts: maxRetries,
+                        scalingDuration: 3000,
+                        excludedStatusCodes: []
+                    })
+                ),
+                catchError(error => {
+                    const { uri } = error.options;
+                    console.log(`Error requesting ${uri} after ${maxRetries} retries.`);
+                    // return null on error
+                    return of(null);
+                }),
+                // filter out errors
+                filter(v => v),
+                // get the cheerio function $
+                map(html => cheerio.load(html)),
+                // add URL to the result. It will be used later for crawling
+                map($ => ({
+                    $,
+                    url
+                }))
+            );
+        },
+        null,
+        maxConcurrentReq
+    ),
+    share()
 );
 
 // crawl all links inside the base URL and send them to allUrl$ to be crawled
 // get all crawable URLs
-urlAndDOM$.subscribe(({ url, $}) => {
+urlAndDOM$.subscribe(({ url, $ }) => {
     $('a').each(function(i, elem) {
         const href = $(this).attr('href');
-        if(!href) return;
+        if (!href) return;
 
-        // build absolute url
+        // build the absolute url
         const absoluteUrl = resolve(url, href);
         allUrl$.next(absoluteUrl);
     });
 });
+
+// scrape for a movie with passed in criteria - customise ratings here
+
+const isMovie = $ =>
+    $(`[property='og:type']`).attr('content') === 'video.movie';
+const isDrama = $ =>
+    $(`.title_wrapper .subtext`)
+        .text()
+        .includes('Drama');
+const isHighlyRated = $ => +$(`[itemprop="ratingValue"]`).text() > 7.5;
+urlAndDOM$
+    .pipe(
+        filter(({ $ }) => isMovie($)),
+        filter(({ $ }) => isDrama($)),
+        filter(({ $ }) => isHighlyRated($))
+    )
+    .subscribe(({ url, $ }) => {
+        // append the data we want to a file named "comedy.txt"
+        fs.appendFile('drama.txt', `${url}, ${$('title').text()}\n`, () => {
+            console.log(`appended ${url}`);
+        });
+    });
